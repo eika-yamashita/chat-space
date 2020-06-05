1e00781a49675dde6a91cf64c260be12e8a95a14
eika-yamashita <skdc0106@gmail.com>
2020-06-05 17:53:51 +0900

    a

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -88,6 +88,6 @@ $('#new_message').on('submit', function(e){
     });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-    setInterval(reloadMessages, 1000);
+    // setInterval(reloadMessages, 1000);
   }
 });
